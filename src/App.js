45ec64d2955137e70482e897ba08ea0fbc59d9dd
amyruth <anrutherford@gmail.com>
2018-09-28 12:30:39 -0400

    first commit; markers added

---
+++ src/App.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import ListSection from './components/ListSection';
+
+class App extends Component {
+	state = {
+		initialCenter: {
+            lat: 35.7454071,
+            lng:-81.68481880000002
+		},
+		myPlaces: []
+	}
+
+	loadMap = () => {
+		loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCc3E8DG6mm62v4R5R3DZFqCn7et6IgxUY&callback=initMap');
+			window.initMap = this.initMap;
+	}
+
+	initMap = () => {
+		let map = new window.google.maps.Map(document.getElementById('map'), {
+			center: this.state.initialCenter,
+			zoom: 15,
+		})
+		console.log('map is loaded');
+
+		// this.state.myPlaces.map( myPlace => {
+
+		// 	 this.marker = new window.google.maps.Marker({
+		// 		position: {
+		// 			lat: myPlace.venue.location.lat,
+		// 			lng: myPlace.venue.location.lng
+		// 		},
+		// 		map: map,
+		// 		title: myPlace.venue.name
+		// 	})
+		// 	return this.marker
+		// })
+
+		this.state.myPlaces.map( myPlace => {
+			this.marker = new window.google.maps.Marker({
+				position: {
+					lat: myPlace.location.lat,
+					lng: myPlace.location.lng
+				},
+				map: map,
+				title: myPlace.name,
+				radius: 20
+			})
+		})
+	}
+
+	componentDidMount() {
+		this.getPlaces();
+	}
+
+getPlaces = () => {
+	const endpoint = 'https://api.foursquare.com/v2/venues/explore';
+	const endpoint2 = 'https://api.foursquare.com/v2/venues/search';
+	
+	axios.get(endpoint2, {
+		params: {
+			client_id:'HLAAAV43L3SOYXNORDN3HSWFR3ZDVSX4PT4HOQKJBW2PQF00',
+			client_secret: 'T2GVYKXUO3HRINYCYFPMYAILORVV4T3LOOWY2N4O5QLERGBC',
+			v: 20180922,
+			ll: '35.7454,-81.6848',
+			// section: 'coffee',
+			query: 'cafe',
+			near: '28655'
+		}			
+	})
+	.then((res) => {
+		console.log(res.status);
+		console.log('places got');
+		this.setState({myPlaces: res.data.response.venues});
+		console.log(this.state.myPlaces);
+	})
+	.then( () => {
+		this.loadMap();
+	})
+	.catch(error => console.log("Error " + error));
+}
+
+	
+	
+  render() {
+	 
+    return (
+      <div className="App">
+
+	  	<ListSection />
+		<div id='map'></div>
+
+      </div>
+    );
+  }
+}
+
+function loadScript(url) {
+	let index = window.document.getElementsByTagName('script')[0];
+	let script = window.document.createElement('script');
+	script.src = url;
+	script.async = true;
+	script.defer = true;
+	index.parentNode.insertBefore(script, index)
+}
+
+export default App;
---
+++ src/components/ListSection.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListSection extends Component {
+	render() {
+		return (
+			<div className='sidebar'>
+				My list
+			</div>
+		);
+	}
+}
+
+export default ListSection;
