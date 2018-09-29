14fabb65dea69380109b3163ce8ff0660e61bb1b
amyruth <anrutherford@gmail.com>
2018-09-29 00:40:04 -0400

    rewrote marker function

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Header = () => {
 	return (
 		<header>
-			<h1>My Morganton Cafe Tour</h1>
-			<h2>Cuz there's nothing much to do here but eat and try to stay awake</h2>
+			<h1>Where to eat in Morganton</h1>
 		</header>
 	);
 };
