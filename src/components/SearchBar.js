1081ef6c283ad8726febe1c05b31e1bf11a173ef
amyruth <anrutherford@gmail.com>
2018-10-01 17:35:37 -0400

    just before adding searchbar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+	render() {
+		return (
+			<div className='search-section'>
+					<label htmlFor='searchbar'>Search Restaurants</label>
+					<input type='text' id='searchbar'
+						value={this.props.searchQuery}
+						onChange={ (e) => this.props.setQuer(e.target.value)} 
+					 />            
+			</div>
+		);
+	}
+}
+
+export default SearchBar;
