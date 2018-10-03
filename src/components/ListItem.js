5f01c253099365d3eae733c406b8b3fec83932f3
amyruth <anrutherford@gmail.com>
2018-10-03 17:28:18 -0400

    filter function working

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const ListItem = props => props.myVenues.map( myVenue =>{
 		return (
 			<li className='listing' key={myVenue.venue.id}
-				
+				title={myVenue.venue.name}
 				onClick={ () => props.listClickHandler(myVenue)}
 			>
 				<p className='venueName'>{myVenue.venue.name}</p>
