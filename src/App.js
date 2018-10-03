ec51806bc47131fc19b3307c413c22eed81256ed
amyruth <anrutherford@gmail.com>
2018-10-03 17:28:18 -0400

    filter function working

--- src/App.js
+++ src/App.js
@@ -16,15 +16,14 @@ class App extends Component {
 		markers: [],
 		listItems: [],
 	}
-
+	
 	listClickHandler = (item) => {
 		console.log('item clicked');
 		console.log(item);
 		console.log(item.venue.name)
 		this.state.markers.forEach(marker => {
 			if(item.venue.id === marker.key) {
-				window.google.maps.event.trigger(marker, 'click');
-					
+				window.google.maps.event.trigger(marker, 'click');		
 			}
 		})
 	}
@@ -32,35 +31,51 @@ class App extends Component {
 	setQuery = (e) => {
 		this.setState({searchQuery: e});
 		console.log(e);
-		this.filterMarkers(e);
+		this.filterResults(e);
 	}
 
-	filterMarkers = (query) => {
-		// grab all list items
-		// convert query and venue name .toLowerCase()
-		// compare them
-		// if not a match add hidden class to list item	
-		// if no match AND class hidden is not present
+	filterResults = (query) => {
 		let listings = document.querySelectorAll('.listing');
-		let copyMarkers = this.state.markers.map( marker => marker)
+		let venues = this.state.myVenues.map(myVenue => myVenue);
+		let copyMarkers = this.state.markers.map( marker => marker);
+
+		query = query.toLowerCase();
+
+		console.log('venues copied', venues);
+		console.log('markers copied', copyMarkers);
+		if(query === '') {
+			listings.forEach( listing => {
+				listing.classList.remove('hidden');
+			})
+			copyMarkers.forEach(marker => marker.setVisible(true));
+			// console.table(copyMarkers);
+		}
 
 		if(query) {
 			query = query.toLowerCase();
 			listings.forEach( listing => {
 				
-				if((listing.innerText.toLowerCase().includes(query) === false) && (listing.classList.contains('hidden') === false)) {
+				if((!listing.title.toLowerCase().includes(query)) ) {
 					listing.classList.add('hidden');	
-					console.log(listing.key)				
+					console.log(listing.title);				
+				} else{
+					listing.classList.remove('hidden');
 				}
 			})
+			
+			copyMarkers.forEach(marker => marker.title.toLowerCase().includes(query) ? 
+				marker.setVisible(true) :
+				marker.setVisible(false)	
+			)
 		}
-		
-		}
+		this.setState({markers: copyMarkers});
+		console.table(this.state.markers);
+	}
 	
 
 
 	loadMap = () => {
-		loadScript('https://maps.googleapis.com/maps/api/js?key=GOOGLE_MAPS&callback=initMap');
+		loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCc3E8DG6mm62v4R5R3DZFqCn7et6IgxUY&callback=initMap');
 			window.initMap = this.initMap;
 	}
 	  
@@ -75,10 +90,10 @@ class App extends Component {
 		let bounds = new window.google.maps.LatLngBounds();
 		let infoWindow = new window.google.maps.InfoWindow();
 		let markers = [];
-		let copyVenues = this.state.myVenues.map(venue => venue);
+		let copyOfVenues = this.state.myVenues.map(venue => venue);
 		
 		//create markers
-		copyVenues.forEach(function(myVenue) {
+		copyOfVenues.forEach(function(myVenue) {
 			let marker = new window.google.maps.Marker({
 				position: {
 					lat: myVenue.venue.location.lat,
@@ -111,41 +126,40 @@ class App extends Component {
 			markers.push(marker);
 		})
 		
-		this.setState({myVenues: copyVenues});
-		this.setState({filteredList: copyVenues});
+		this.setState({myVenues: copyOfVenues});
+		this.setState({filteredList: copyOfVenues});
 		this.setState({ markers: markers });
-}
-
+	}
 
 	componentDidMount() {
 		this.getPlaces();
 	}
 
-getPlaces = () => {
-	const endpoint = 'https://api.foursquare.com/v2/venues/explore';
-	console.log('grabbing locations');
-	axios.get(endpoint, {
-		params: {
-			client_id: '4SQUARE',
-			client_secret: '4SQUARE',
-			v: 20180922,
-			ll: '35.7454,-81.6848',
-			section: 'food',
-			near: 'Morganton'
-		}			
-	})
-	.then((res) => {
-		console.log("Response from server: " + res.status);
-		console.log('locations retrieved');
-		// console.log(res.data.response.groups[0].items);
-		this.setState({myVenues: res.data.response.groups[0].items});
-		// console.log(this.state.myVenues);
-	})
-	.then( () => {
-		this.loadMap();
-	})
-	.catch(error => console.log("Error " + error));
-}
+	getPlaces = () => {
+		const endpoint = 'https://api.foursquare.com/v2/venues/explore';
+		console.log('grabbing locations');
+		axios.get(endpoint, {
+			params: {
+				client_id: 'HLAAAV43L3SOYXNORDN3HSWFR3ZDVSX4PT4HOQKJBW2PQF00',
+				client_secret: 'T2GVYKXUO3HRINYCYFPMYAILORVV4T3LOOWY2N4O5QLERGBC',
+				v: 20180922,
+				ll: '35.7454,-81.6848',
+				section: 'food',
+				near: 'Morganton'
+			}			
+		})
+		.then((res) => {
+			console.log("Response from server: " + res.status);
+			console.log('locations retrieved');
+			console.log(res.data.response.groups[0].items);
+			this.setState({myVenues: res.data.response.groups[0].items});
+			// console.log(this.state.myVenues);
+		})
+		.then( () => {
+			this.loadMap();
+		})
+		.catch(error => console.log("Error " + error));
+	}
 
 
 
@@ -164,7 +178,7 @@ getPlaces = () => {
 				searchQuery={this.state.searchQuery}
 				setQuery={this.setQuery}
 				value={this.state.searchQuery}
-				filterMarkers={this.filterMarkers}
+				filterResults={this.filterResults}
 				listItems={this.state.listItems}
 			/>
 			<div id='map'></div>
