e118c7ad38f33e9c8e4a9a9614c28c14f69b59a8
amyruth <anrutherford@gmail.com>
2018-10-07 05:05:20 -0400

    sidebar no longer collapses when filtering results

--- src/components/VenueList.js
+++ src/components/VenueList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ListItem from './ListItem';
-
+import './VenueList.css';
 const VenueList = props => {
 	return (
 		<ul className='venues'>
