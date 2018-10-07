a2601611bc4518e3ec58860260d7948f11b1fb25
amyruth <anrutherford@gmail.com>
2018-10-07 04:45:36 -0400

    map does not scroll; work on menu next

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -11,7 +11,7 @@ const ListItem = props => props.myVenues.map( myVenue =>{
 			>				<p className='venueName'>{myVenue.venue.name}</p>
 				
 				<p>Category: {myVenue.venue.categories[0].shortName}</p>	
-				<img src={"https://maps.googleapis.com/maps/api/streetview?size=175x175&location=" + myVenue.venue.location.lat + "," + myVenue.venue.location.lng + "&key=***REMOVED***"}/>
+				
 			</li>
 		)
 		
