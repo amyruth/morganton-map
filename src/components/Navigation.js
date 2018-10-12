d9f3f28f37c1b94a55599d9bcf3763ad62b84939
amyruth <anrutherford@gmail.com>
2018-10-12 18:42:27 -0400

    Removed searchbar component; if it aint broke dont fix it

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import VenueList from './VenueList';
 import './Navigation.css';
 
+
 export default class Navigation extends Component {
 
 	render() {
@@ -19,7 +20,8 @@ export default class Navigation extends Component {
 
 				<div className='search-section'>
 					
-						<input type='text' id='searchbar'
+						<input type='text' 
+							id='searchbar'
 							aria-label='Search restaurants by name'
 							role='search'
 							placeholder='Search places by name' 
@@ -27,7 +29,7 @@ export default class Navigation extends Component {
 							onChange={(e) => this.props.setQuery(e.target.value)} 
 						/>
                 </div>
-
+			
 				<a className="attribution" href='https://foursquare.com/' target='blank'>Powered by Foursquare</a>
 
 				<VenueList myVenues={this.props.myVenues}
