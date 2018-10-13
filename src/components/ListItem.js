89d708951b39374ab37f061c66d5a7d1aa765453
amyruth <anrutherford@gmail.com>
2018-10-13 19:14:14 -0400

    removed img from list; have to adjust css

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -14,7 +14,7 @@ const ListItem = props => props.myVenues.map( myVenue =>{
 					<p className='venueName'>
 						{myVenue.venue.name}		
 					</p>
-					<img src={"https://maps.googleapis.com/maps/api/streetview?size=125x125&location=" + myVenue.venue.location.lat + "," + myVenue.venue.location.lng + "&key=***REMOVED***"} className='venueImage' alt={myVenue.venue.name +", " + myVenue.venue.location.address} />
+					{/* <img src={"https://maps.googleapis.com/maps/api/streetview?size=125x125&location=" + myVenue.venue.location.lat + "," + myVenue.venue.location.lng + "&key=***REMOVED***"} className='venueImage' alt={myVenue.venue.name +", " + myVenue.venue.location.address} /> */}
 				</div>
 			</li>
 		)
