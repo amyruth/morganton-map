4e29a896a043ce31a08f2de5178f57a725ca1837
amyruth <anrutherford@gmail.com>
2018-10-14 17:53:42 -0400

    added license and referenced it in readme

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ export default class App extends Component {
 	}
 
 	loadMap = () => {
-		loadScript('https://maps.googleapis.com/maps/api/js?key=GOOGLEAPIKEY&callback=initMap');
+		loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCc3E8DG6mm62v4R5R3DZFqCn7et6IgxUY&callback=initMap');
 			window.initMap = this.initMap;
 	}
 
@@ -189,8 +189,8 @@ export default class App extends Component {
 		// console.log('grabbing locations');
 		axios.get(endpoint, {
 			params: {
-				client_id: '4SQUAREID',
-				client_secret: '4SQUARESECRET',
+				client_id: 'HLAAAV43L3SOYXNORDN3HSWFR3ZDVSX4PT4HOQKJBW2PQF00',
+				client_secret: 'T2GVYKXUO3HRINYCYFPMYAILORVV4T3LOOWY2N4O5QLERGBC',
 				v: 20180922,
 				ll: '35.7454,-81.6848',
 				section: 'food',
