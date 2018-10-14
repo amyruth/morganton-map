19a8a22e6649844ac091203f2c5451c91512dc9d
amyruth <anrutherford@gmail.com>
2018-10-14 17:33:55 -0400

    commented out console statements, removed some unused CSS, added readme

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const ListItem = props => props.myVenues.map( myVenue =>{
 		return (
@@ -8,17 +7,15 @@ const ListItem = props => props.myVenues.map( myVenue =>{
 				title={myVenue.venue.name}
 				onClick={ () => props.listClickHandler(myVenue)}
 				onKeyDown={(e) => {props.listKbHandler(e, myVenue)}}
-				tabIndex='-1'
+				tabIndex='0'
 			>	
 				<div className='venue-listing' role='menuitem'>
 					<p className='venueName'>
 						{myVenue.venue.name}		
 					</p>
-					{/* <img src={"https://maps.googleapis.com/maps/api/streetview?size=125x125&location=" + myVenue.venue.location.lat + "," + myVenue.venue.location.lng + "&key=AIzaSyCc3E8DG6mm62v4R5R3DZFqCn7et6IgxUY"} className='venueImage' alt={myVenue.venue.name +", " + myVenue.venue.location.address} /> */}
 				</div>
 			</li>
-		)
-		
+		)		
 });
 
 export default ListItem;
